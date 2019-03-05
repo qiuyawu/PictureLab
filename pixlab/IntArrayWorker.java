0cf819c0227d0fe3f3626209b0e4dd2a90ba9807
qiuyawu <43949863+qiuyawu@users.noreply.github.com>
2019-03-05 12:27:36 -0800

    Day 1

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -98,5 +98,15 @@ public class IntArrayWorker
       }
     }
   }
+  
+  public int getCount(int n)
+  {   
+      for (int i=0; i<matrix.length; i++)
+      {
+          for (int j=0; j<matrix[0].length; j++)
+          {
+              if (
+              matrix[i][j] 
+      return -1;
  
 }
